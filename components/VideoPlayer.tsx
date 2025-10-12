08baa9fc791f3cf7cab76d590d90df766bd2201a
marfandre <amarphichev@gmail.com>
2025-10-12 21:08:51 +0300

    gg

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -10,8 +10,7 @@ export default function VideoPlayer({ playbackId }: Props) {
       streamType="on-demand"
       playbackId={playbackId}
       playsInline
-      controls
-      // опционально:
+      // Если нужен автоплей:
       // autoPlay
       // muted
       style={{ width: '100%', aspectRatio: '16 / 9' }}
